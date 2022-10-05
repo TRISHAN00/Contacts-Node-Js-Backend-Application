782ae3f38133e9e4af5a0adfe323c93d038a37c3
TRISHAN00 <trishansaha43@gmail.com>
2022-10-05 13:07:08 +0600

    completed till updated Contact

--- Contacts.js
+++ Contacts.js
@@ -12,6 +12,22 @@ class Contacts {
     this.contacts.push(contact);
     return contact;
   }
+
+  getSingleContactById(id) {
+    return this.contacts.find((contact) => contact.id === id);
+  }
+
+  updateContactById(id) {
+    const index = this.contacts.findIndex((contact) => contact.id === id);
+    let updatedContact = this.contacts[index];
+    this.contacts[index].name =
+      updatedContact.name || this.contacts[index].name;
+    this.contacts[index].email =
+      updatedContact.name || this.contacts[index].email;
+    this.contacts[index].phone =
+      updatedContact.name || this.contacts[index].phone;
+    return updatedContact;
+  }
 }
 
 module.exports = new Contacts();
