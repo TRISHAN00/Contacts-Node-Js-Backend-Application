6402b77c507b26dee5c739ef3c6843c65b29d4e4
TRISHAN00 <trishansaha43@gmail.com>
2022-10-11 13:50:29 +0600

    basic setup for template engine

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.join());
+
+app.get("/", (req, res) => {
+  res.send("Working with Template Engine");
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
