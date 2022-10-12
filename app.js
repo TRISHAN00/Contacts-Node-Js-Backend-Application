40f98e03d537ee8f69f7c65aef1337b0420c9c11
TRISHAN00 <trishansaha43@gmail.com>
2022-10-13 01:49:12 +0600

    add navigation system

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+app.set("view engine", "ejs");
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.set("view engine", "ejs");
+app.get("/about", (req, res) => {
+  res.render("pages/about");
+});
+
+app.get("/help", (req, res) => {
+  res.render("pages/help");
+});
 
 app.get("/", (req, res) => {
   let post = {
@@ -22,7 +29,7 @@ app.get("/", (req, res) => {
     { title: "Top 1 News", author: "Rishan" },
   ];
 
-  res.render("index", { serial: "second", post, posts });
+  res.render("pages/index", { serial: "second", post, posts });
 });
 
 const PORT = process.env.PORT || 8080;
