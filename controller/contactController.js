1e6f22c30cbda30024a6ea1fb71d29f5af95a289
TRISHAN00 <trishansaha43@gmail.com>
2022-11-17 00:50:57 +0600

    update contact

--- controller/contactController.js
+++ controller/contactController.js
@@ -23,13 +23,29 @@ exports.createContact = (req, res) => {
     });
 };
 
-exports.getAllContacts = (req, res) => {
+exports.getAllContacts = (_req, res) => {
   Contact.find()
     .then((contacts) => {
       res.json(contacts);
     })
     .catch((e) => {
       console.log(r);
+      res.json({
+        message: "Error occurred!",
+      });
+    });
+};
+
+exports.updateContact = (req, res) => {
+  let { name, email, phone } = req.body;
+  let { id } = req.params;
+  console.log(id, name, email, phone);
+  Contact.findOneAndUpdate({ _id: id }, { name, email, phone }, { new: true })
+    .then((contact) => {
+      res.json(contact);
+    })
+    .catch((e) => {
+      console.log(e);
       res.json({
         message: "Error occurred!",
       });
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,11 +1,13 @@
 const {
   createContact,
   getAllContacts,
+  updateContact,
 } = require("../controller/contactController");
 
 const router = require("express").Router();
 
 router.post("/", createContact);
 router.get("/", getAllContacts);
+router.put("/:id", updateContact);
 
 module.exports = router;
