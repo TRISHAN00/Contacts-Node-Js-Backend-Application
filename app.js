3c7822fa7e38b7390656e004f4b137143118e1fc
TRISHAN00 <trishansaha43@gmail.com>
2022-11-17 00:00:37 +0600

    listen port number run server

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
-const express = require("express");
-const app = express("express");
 const mongoose = require("mongoose");
+const express = require("express");
 const morgan = require("morgan");
-const router = require("./routes");
 
-app.set("view engine", "ejs");
-app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use("/contacts", router);
+const app = express("");
 
-// Schema declaration
-app.get("/", (_req, res) => {
-  res.json({
-    message: "Welcome",
-  });
-});
-
-// database and server connection
 const PORT = process.env.PORT || 8080;
-mongoose
-  .connect(`mongodb://127.0.0.1:27017/contacts`)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`server start on port ${PORT}`);
-    });
-  })
-  .catch((e) => {
-    console.log(e);
-  });
+app.listen(PORT, () => {
+  console.log(`Server will listen on port ${PORT}`);
+});
